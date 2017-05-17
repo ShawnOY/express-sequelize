4def8bf4b30583b8a29257a557927f99c8663fec
Shawn OY <shawnoy@outlook.com>
2017-05-17 14:11:31 +0800

    Add some templates

--- routes/master.js
+++ routes/master.js
@@ -8,7 +8,17 @@ router.route('/')
   .get((request, response) => {
     models.Master.findAll()
       .then(masters => {
-        response.json(masters)
+        response.render('master/index', {masters: masters})
+      })
+  })
+
+router.route('/:masterId')
+  .get((request, response) => {
+    models.Master.findById(request.params.masterId, {
+      include: [models.Pet]
+    })
+      .then(master => {
+        response.render('master/show', {master: master})
       })
   })
 
