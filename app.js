f2b363d8911c91f6b27acce07d5c02697481137d
Shawn OY <shawnoy@outlook.com>
2017-07-19 09:00:44 +0800

    Modify routes

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 const master = require('./routes/master')
 
 var app = express();
@@ -23,8 +21,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
 app.use('/master', master)
 
 // catch 404 and forward to error handler
--- routes/master.js
+++ routes/master.js
@@ -12,16 +12,6 @@ router.route('/')
       })
   })
 
-router.route('/:masterId')
-  .get((request, response) => {
-    models.Master.findById(request.params.masterId, {
-      include: [models.Pet]
-    })
-      .then(master => {
-        response.render('master/show', {master: master})
-      })
-  })
-
 router.route('/create')
   .get((request, response) => {
     response.render('master/create')
@@ -31,8 +21,30 @@ router.route('/create')
       name: request.body.name
     })
       .then(() => {
-        console.log('enter')
         response.redirect('/master')
+      })
+  })
+
+router.route('/:masterId/pet')
+  .get((request, response) => {
+    models.Master.findById(request.params.masterId, {
+      include: [models.Pet]
+    })
+      .then(master => {
+        response.render('master/show', {master: master})
+      })
+  })
+router.route('/:masterId/pet/create')
+  .get((request, response) => {
+    response.render('pet/create')
+  })
+  .post((request, response) => {
+    models.Pet.create({
+      name: request.body.name,
+      MasterId: request.params.masterId
+    })
+      .then(() => {
+        response.redirect('/master/' + request.params.masterId + '/pet')
       })
   })
 
