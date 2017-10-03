4261d2c712f0a5f0a3e0b3982b4700dc93ba3a2a
Shawn OY <shawnoy@outlook.com>
2017-10-03 13:05:36 +0800

    Add add master page

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,31 @@ const express = require('express')
 const router = express.Router()
 const models = require('../models')
 
-router.get('/', function(request, response, next) {
+router.get('/', (request, response) => {
   models.Master.findAll()
   .then(masters => {
     response.render('index', {masters: masters})
+  })
+})
+
+router.route('/master')
+.get((request, response) => {
+  response.render('masterForm')
+})
+.post((request, response) => {
+  const postData = request.body
+
+  models.Master.create({
+    name: postData.masterName
+  })
+  .then(master => {
+    return models.Pet.create({
+      name: postData.petName,
+      MasterId: master.id
+    })
+  })
+  .then(pet => {
+    response.redirect('/')
   })
 })
 
